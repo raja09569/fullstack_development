fc89ad33c56a2c29d04cfe2f4e15bbd256c30f6c
rajashekar makam <rajashekar.pandu@gmail.com>
2021-04-25 15:59:38 +0530

    code updated

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 #!/usr/bin/node
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 var shell = require('shelljs');
 
-const questions = [{
+// slice argv as we don't need the forst two elements (in this case)
+const args = process.argv.slice(2, process.argv.length);
+console.log(args);
+const projectName = args[0];
+
+if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+}
+
+//Creating directory
+shell.mkdir(projectName);
+
+// Copy files to release dir
+
+//Change direcotry
+shell.cd(projectName);
+
+//Create package.json file with project name
+shell.exec("npm set init.author.email 'rajashekar0569@gmail.com'");
+shell.exec("npm set init.author.name 'Rajashekar Makam'");
+shell.exec("npm set init.license 'MIT'");
+shell.exec("npm init -y");
+
+
+/* const questions = [{
         type: "input",
         name: "projectName",
         message: "It's a Next.js, Redux, styled-Components template. Please enter your new project's name.",
@@ -22,10 +47,7 @@ inquirer.prompt(questions).then(answers => {
     shell.mkdir(projectName);
  
     // Copy files to release dir
-    /* shell.rm('-rf', projectName+'/*');
-    shell.cp('-R', 'client/', projectName+'/');
-    shell.cp('-R', 'server/', projectName+'/');
- */
+   
     //Change direcotry
     shell.cd(projectName);
 
@@ -39,7 +61,7 @@ inquirer.prompt(questions).then(answers => {
     //add script command
     // shell.sed('-i', 'scripts', '', 'package.json');
       
-});
+}); */
 
 /* 
 var shell = require('shelljs');
